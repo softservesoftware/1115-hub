0779be138d42764de3b1b0fb09de95c7a3e517ac
mdvertola <mdvertola@gmail.com>
2024-03-08 07:00:57 -0500

    testing ecs deployment

--- support/infrastructure/bin/infrastructure.ts
+++ support/infrastructure/bin/infrastructure.ts
@@ -1,118 +1,19 @@
-#!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { Stack, StackProps } from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import { Construct } from "constructs";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as efs from "aws-cdk-lib/aws-efs";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecrAssets from "aws-cdk-lib/aws-ecr-assets";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { Construct } from "constructs";
+import { App } from "aws-cdk-lib";
 
-interface ComputeStackProps extends StackProps {
-  vpc: ec2.Vpc;
-  eip: ec2.CfnEIP;
-  fileSystem: efs.FileSystem;
-}
-
-class ComputeStack extends Stack {
-  readonly instance: ec2.Instance;
-
-  constructor(scope: Construct, id: string, props: ComputeStackProps) {
-    super(scope, id, props);
-
-    // Security Group for compute EC2
-    const ec2SecurityGroup = new ec2.SecurityGroup(this, "Ec2SecurityGroup", {
-      vpc: props.vpc,
-      description: "Security group for compute EC2 instance",
-      allowAllOutbound: true,
-    });
-
-    // Allow SCP/SFTP access on port 2222 from any IP
-    ec2SecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(2222),
-      "Allow SCP/SFTP access on port 2222 from any IP",
-    );
-
-    // Allow SSH access from a specific IP range, all for now
-    ec2SecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(22),
-      "Allow SSH access from a specific block",
-    );
-    ec2SecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(443),
-      "Allows HTTPS access from Internet",
-    );
-
-    // IAM Role for the EC2 Instance
-    const role = new iam.Role(this, "Ec2Role", {
-      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
-      // Add necessary managed policies or inline policies here
-    });
-
-    const userData = ec2.UserData.forLinux();
-    // run commands on the instance for initial setup
-
-    // create random string, forces the instance to run the commands on every deployment
-    // less than ideal but works for now (until proper containerized deployment is implemented)
-    // avoids the need of separate stacks and cdk destroy/ deploy on instance changes
-    const randomString = Math.floor(Date.now() / 1000);
-
-    userData.addCommands(
-      `echo deployment: ${randomString} > /etc/deployment.txt`,
-      "apt-get update -y",
-      "apt-get install -y ca-certificates curl nfs-common",
-      "install -m 0755 -d /etc/apt/keyrings",
-      "curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc",
-      "chmod a+r /etc/apt/keyrings/docker.asc",
-      `echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null`,
-      "apt-get update",
-      "apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
-      "curl -Ssf https://pkgx.sh | sh",
-      "install -m 755 pkgx /usr/local/bin",
-      "export PATH=$PATH:/home/admin/.local/bin",
-      "pkgx install git",
-      "export PATH=$PATH:/home/admin/.local/bin",
-      "mkdir -p 1115-hub/support/infrastructure/containers",
-      `mount -t nfs4 -o nfsvers=4.1 ${props.fileSystem.fileSystemId}.efs.us-east-1.amazonaws.com:/ 1115-hub/support/infrastructure/containers`,
-      "mkdir -p 1115-hub/support/infrastructure/containers/example",
-      "git clone https://github.com/qe-collaborative-services/1115-hub.git 1115-hub/support/infrastructure/containers",
-      "cd 1115-hub/support/infrastructure/containers",
-      "docker compose up --build",
-    );
-
-    // EC2 Instance
-    this.instance = new ec2.Instance(this, "ElevenFifteenComputeInstance", {
-      vpc: props.vpc,
-      instanceType: new ec2.InstanceType("t3.micro"),
-      machineImage: ec2.MachineImage.genericLinux({
-        "us-east-1": "ami-0133fb3dded749b65", // debian bullseye latest amd64
-        // ...add other regions if necessary
-        // view other ids here: https://wiki.debian.org/Cloud/AmazonEC2Image
-      }),
-      securityGroup: ec2SecurityGroup,
-      role: role,
-      userData: userData,
-      // keyName is a temporary solution for testing
-      // keyName: "keys",
-      // should use a key pair for production (or not include to block ssh access)
-      // keyPair: new ec2.KeyPair(this, "ComputeInstanceKeyPair", {}),
-      vpcSubnets: {
-        subnetGroupName: "compute-subnet",
-      },
-    });
-    // Associate the Elastic IP with the EC2 Instance
-    new ec2.CfnEIPAssociation(this, "EIPAssociation", {
-      eip: props.eip.ref, // Reference to the EIP resource
-      instanceId: this.instance.instanceId,
-    });
-  }
-}
 export interface networkStackProps extends cdk.StackProps {
 }
-class networkStack extends cdk.Stack {
+export class networkStack extends cdk.Stack {
   readonly vpc: ec2.Vpc;
   readonly eip: ec2.CfnEIP;
+  readonly sftpListener: elbv2.NetworkListener;
   constructor(scope: Construct, id: string, props: networkStackProps) {
     super(scope, id, props);
     // create a vpc that we can put an ec2 and rds instance into
@@ -134,41 +35,178 @@ class networkStack extends cdk.Stack {
       ],
     });
 
-    // Allocate an Elastic IP
-    this.eip = new ec2.CfnEIP(this, "EIP");
+    // Create a Network Load Balancer
+    const nlb = new elbv2.NetworkLoadBalancer(this, "Nlb", {
+      vpc: this.vpc,
+      internetFacing: true,
+    });
 
-    // create export for the EIP
-    new cdk.CfnOutput(this, "instanceIP", {
-      value: this.eip.ref,
-      description: "The Elastic IP for the compute instance",
+    // create listener for sftp
+    this.sftpListener = nlb.addListener("sftpListener", {
+      port: 22,
     });
   }
 }
 
-export interface dataStackProps extends cdk.StackProps {
+export interface DataStackProps extends cdk.StackProps {
   vpc: ec2.Vpc;
 }
 
 export class DataStack extends cdk.Stack {
   readonly fileSystem: efs.FileSystem;
-  constructor(scope: Construct, id: string, props: dataStackProps) {
+  constructor(scope: Construct, id: string, props: DataStackProps) {
     super(scope, id, props);
-    // Create an EFS file system
-    this.fileSystem = new efs.FileSystem(this, "FileSystem", {
+
+    // Define the EFS filesystem
+    this.fileSystem = new efs.FileSystem(this, "ElevenFifteenEfs", {
       vpc: props.vpc,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
   }
 }
 
-const app = new cdk.App();
+export interface ComputeStackProps extends cdk.StackProps {
+  vpc: ec2.Vpc;
+  eip: ec2.CfnEIP;
+  fileSystem: efs.FileSystem;
+  sftpListener: elbv2.NetworkListener;
+}
+
+export class ComputeStack extends cdk.Stack {
+  readonly instance: ec2.Instance;
+
+  constructor(scope: Construct, id: string, props: ComputeStackProps) {
+    super(scope, id, props);
+
+    // Create an ECS cluster
+    const cluster = new ecs.Cluster(this, "ElevenFifteenCluster", {
+      vpc: props.vpc,
+    });
+
+    // create the sftp task definition
+    const sftpTaskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      "sftpTaskDef",
+      {
+        memoryLimitMiB: 512,
+        cpu: 256,
+      },
+    );
+    // add the efs volume to the task definition
+    sftpTaskDefinition.addVolume({
+      name: "sftp_data",
+      efsVolumeConfiguration: {
+        fileSystemId: props.fileSystem.fileSystemId,
+      },
+    });
+
+    // create the workflow task definition
+    const workflowTaskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      "workflowTaskDef",
+      {
+        memoryLimitMiB: 1024,
+        cpu: 512,
+      },
+    );
+    // add the efs volume to the task definition
+    workflowTaskDefinition.addVolume({
+      name: "sftp_data",
+      efsVolumeConfiguration: {
+        fileSystemId: props.fileSystem.fileSystemId,
+      },
+    });
+
+    // create the SFTP docker image & add to task definition
+    const sftpContainer = sftpTaskDefinition.addContainer("sftpContainer", {
+      image: ecs.ContainerImage.fromRegistry("atmoz/sftp"),
+      command: [
+        "qe1:pass:::ingress/",
+        "qe2:pass:::ingress/",
+        "qe3:pass:::ingress/",
+        "qe4:pass:::ingress/",
+        "qe5:pass:::ingress/",
+        "qe6:pass:::ingress/",
+      ],
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: "sftp" }),
+      portMappings: [{ containerPort: 22, hostPort: 22 }],
+    });
+
+    sftpContainer.addMountPoints({
+      containerPath: "/home",
+      sourceVolume: "sftp_data",
+      readOnly: false,
+    });
+
+    // Define a Docker image asset for the workflow container
+    const workflowDockerImage = new ecrAssets.DockerImageAsset(
+      this,
+      "workflowImage",
+      {
+        directory: "./containers/", // Adjust this to the path of your Docker context
+        file: "Dockerfile.workflow", // Specify the Dockerfile name
+        buildArgs: {
+          REPO_URL: "https://github.com/qe-collaborative-services/1115-hub.git",
+        },
+      },
+    );
+
+    // Add the workflow container using the image from ECR
+    const workflowContainer = workflowTaskDefinition.addContainer(
+      "workflowContainer",
+      {
+        image: ecs.ContainerImage.fromDockerImageAsset(workflowDockerImage),
+        logging: ecs.LogDrivers.awsLogs({ streamPrefix: "workflow" }),
+      },
+    );
+
+    workflowContainer.addMountPoints({
+      containerPath: "/SFTP",
+      sourceVolume: "sftp_data",
+      readOnly: false,
+    });
+
+    // create the sftp service
+    const sftpService = new ecs.FargateService(this, "SftpService", {
+      cluster,
+      taskDefinition: sftpTaskDefinition,
+      desiredCount: 1, // Adjust based on your needs
+    });
+
+    props.sftpListener.addTargets("sftpService", {
+      port: 22,
+      targets: [sftpService],
+      healthCheck: {
+        interval: cdk.Duration.seconds(240),
+        timeout: cdk.Duration.seconds(60),
+        healthyThresholdCount: 2,
+        unhealthyThresholdCount: 2,
+        protocol: elbv2.Protocol.TCP,
+      },
+    });
+
+    // create the workflow service
+    const workflowService = new ecs.FargateService(this, "WorkflowService", {
+      cluster,
+      taskDefinition: workflowTaskDefinition,
+      desiredCount: 1, // Adjust based on your needs
+    });
+  }
+}
+
+const app = new App();
 const network = new networkStack(
   app,
   `${process.env.ENV}ElevenFifteenNetwork`,
   {},
 );
-const data = new DataStack(app, `${process.env.ENV}ElevenFifteenData`, {
-  vpc: network.vpc,
-});
+const data = new DataStack(
+  app,
+  `${process.env.ENV}ElevenFifteenData`,
+  {
+    vpc: network.vpc,
+  },
+);
 const compute = new ComputeStack(
   app,
   `${process.env.ENV}ElevenFifteenCompute`,
@@ -176,6 +214,7 @@ const compute = new ComputeStack(
     vpc: network.vpc,
     eip: network.eip,
     fileSystem: data.fileSystem,
+    sftpListener: network.sftpListener,
   },
 );
 
