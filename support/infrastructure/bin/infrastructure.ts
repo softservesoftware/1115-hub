f9a2d49197df9cccbb8362ee3e56f6d5497d037f
mdvertola <mdvertola@gmail.com>
2024-03-27 01:19:28 -0400

    adding in functional ecs deployment

--- support/infrastructure/bin/infrastructure.ts
+++ support/infrastructure/bin/infrastructure.ts
@@ -3,17 +3,11 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as efs from "aws-cdk-lib/aws-efs";
 import * as ecs from "aws-cdk-lib/aws-ecs";
-import * as logs from "aws-cdk-lib/aws-logs";
 import * as ecrAssets from "aws-cdk-lib/aws-ecr-assets";
-import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
-import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
-import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import { ManagedPolicy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 import { App } from "aws-cdk-lib";
-import { RetentionDays } from "aws-cdk-lib/aws-logs";
-import { FlowLogTrafficType } from "aws-cdk-lib/aws-ec2";
 
 export interface ComputeStackProps extends cdk.StackProps {}
 
@@ -23,8 +17,89 @@ export class ComputeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: ComputeStackProps) {
     super(scope, id, props);
 
+    //
+    //
+    // Shared Services
+    //
+    //
+
+    // create the VPC
     const vpc = new ec2.Vpc(this, "VPC", { maxAzs: 2 });
-    const cluster = new ecs.Cluster(this, "Cluster", { vpc });
+
+    // create the ECS cluster
+    const cluster = new ecs.Cluster(this, "Cluster", {
+      vpc: vpc,
+      containerInsights: true,
+    });
+
+    // Create the EFS filesystem
+    const fileSystem = new efs.FileSystem(this, "SharedEfsFileSystem", {
+      vpc,
+      encrypted: true,
+      lifecyclePolicy: efs.LifecyclePolicy.AFTER_14_DAYS, // Adjust according to your needs
+      performanceMode: efs.PerformanceMode.GENERAL_PURPOSE,
+      throughputMode: efs.ThroughputMode.BURSTING,
+    });
+
+    const accessPoint = new efs.AccessPoint(this, "AccessPoint", {
+      fileSystem: fileSystem,
+      path: "/",
+    });
+
+    // Allow ECS tasks to connect to the EFS filesystem
+    fileSystem.connections.allowDefaultPortFrom(cluster.connections);
+    fileSystem.connections.allowDefaultPortTo(cluster.connections);
+
+    // Define the EFS volume for ECS tasks
+    const efsVolume: ecs.Volume = {
+      name: "efsVolume",
+      efsVolumeConfiguration: {
+        fileSystemId: fileSystem.fileSystemId,
+        transitEncryption: "ENABLED",
+        authorizationConfig: {
+          accessPointId: accessPoint.accessPointId,
+        },
+      },
+    };
+
+    // efs access policy that we can add to our ECS task roles
+    const efsAccessPolicy = new ManagedPolicy(this, "efsAccessPolicy", {
+      statements: [
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: ["elasticfilesystem:*"],
+          resources: [fileSystem.fileSystemArn],
+        }),
+      ],
+    });
+
+    //
+    //
+    // Workflow Service
+    //
+    //
+
+    // create a role for workflow tasks to access the EFS filesystem
+    const workflowTaskRole = new iam.Role(this, "workflowTaskRole", {
+      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+      managedPolicies: [efsAccessPolicy],
+    });
+
+    // create a security group for the workflow service
+    const workflowSg = new ec2.SecurityGroup(
+      this,
+      "workflowServiceSecurityGroup",
+      {
+        vpc,
+        allowAllOutbound: true,
+      }
+    );
+    // allow inbound traffic to the workflow service from the EFS filesystem
+    workflowSg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(efs.FileSystem.DEFAULT_PORT),
+      "Allow inbound traffic to the workflow service from the EFS filesystem"
+    );
 
     // Define a Docker image asset for the workflow container
     const workflowDockerImage = new ecrAssets.DockerImageAsset(
@@ -53,13 +128,47 @@ export class ComputeStack extends cdk.Stack {
           taskImageOptions: {
             image: ecs.ContainerImage.fromDockerImageAsset(workflowDockerImage),
             enableLogging: true,
-            containerPort: 8081,
+            containerPort: 8082,
+            taskRole: workflowTaskRole,
           },
           publicLoadBalancer: false,
-          listenerPort: 8081,
+          listenerPort: 8082,
           healthCheckGracePeriod: cdk.Duration.seconds(300),
+          securityGroups: [workflowSg],
+          enableExecuteCommand: true,
         }
       );
+
+    // allow inbound & outbound traffic between the workflow service & file system
+    workflowService.service.connections.allowFrom(
+      fileSystem,
+      ec2.Port.tcp(efs.FileSystem.DEFAULT_PORT)
+    );
+    workflowService.service.connections.allowTo(
+      fileSystem,
+      ec2.Port.tcp(efs.FileSystem.DEFAULT_PORT)
+    );
+    fileSystem.connections.allowDefaultPortFrom(workflowService.service);
+    fileSystem.connections.allowDefaultPortTo(workflowService.service);
+
+    // add the EFS volume to the workflow service task definition
+    workflowService.taskDefinition.addVolume({
+      name: "efsVolume",
+      efsVolumeConfiguration: {
+        fileSystemId: fileSystem.fileSystemId,
+        transitEncryption: "ENABLED",
+        authorizationConfig: {
+          accessPointId: accessPoint.accessPointId,
+          iam: "ENABLED",
+        },
+      },
+    });
+    // add the mount point to the container definition
+    workflowService.taskDefinition.defaultContainer!.addMountPoints({
+      containerPath: "/SFTP",
+      sourceVolume: "efsVolume",
+      readOnly: false,
+    });
 
     // Setup AutoScaling policy
     const workflowServiceScaling = workflowService.service.autoScaleTaskCount({
@@ -70,6 +179,30 @@ export class ComputeStack extends cdk.Stack {
       scaleInCooldown: cdk.Duration.seconds(60),
       scaleOutCooldown: cdk.Duration.seconds(60),
     });
+
+    //
+    //
+    // SFTP Service
+    //
+    //
+    // create a role for sftp tasks to access the EFS filesystem
+    const sftpTaskRole = new iam.Role(this, "sftpTaskRole", {
+      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+      managedPolicies: [efsAccessPolicy],
+    });
+
+    // create a security group for the sftp service
+    const sftpSg = new ec2.SecurityGroup(this, "sftpServiceSecurityGroup", {
+      vpc,
+      allowAllOutbound: true,
+    });
+
+    // allow inbound traffic to the sftp service from the EFS filesystem
+    sftpSg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(efs.FileSystem.DEFAULT_PORT),
+      "Allow inbound traffic to the sftp service from the EFS filesystem"
+    );
 
     // create a load-balanced Fargate service for the sftp container
     const sftpDockerImage = new ecrAssets.DockerImageAsset(this, "sftpImage", {
@@ -90,12 +223,49 @@ export class ComputeStack extends cdk.Stack {
           image: ecs.ContainerImage.fromDockerImageAsset(sftpDockerImage),
           enableLogging: true,
           containerPort: 22,
+          taskRole: sftpTaskRole,
         },
         publicLoadBalancer: true,
-        listenerPort: 22,
         healthCheckGracePeriod: cdk.Duration.seconds(300),
+        listenerPort: 22,
       }
     );
+
+    // allow inbound & outbound traffic between the workflow service & file system
+    sftpService.service.connections.allowFrom(
+      fileSystem,
+      ec2.Port.tcp(efs.FileSystem.DEFAULT_PORT)
+    );
+    sftpService.service.connections.allowTo(
+      fileSystem,
+      ec2.Port.tcp(efs.FileSystem.DEFAULT_PORT)
+    );
+    fileSystem.connections.allowDefaultPortFrom(sftpService.service);
+    fileSystem.connections.allowDefaultPortTo(sftpService.service);
+
+    // allow inbound traffic to the sftp service
+    sftpService.service.connections.allowFromAnyIpv4(ec2.Port.tcp(22));
+    // allow inbound traffic to the load balancer
+    sftpService.loadBalancer.connections.allowFromAnyIpv4(ec2.Port.tcp(22));
+
+    // add the EFS volume to the workflow service task definition
+    sftpService.taskDefinition.addVolume({
+      name: "efsVolume",
+      efsVolumeConfiguration: {
+        fileSystemId: fileSystem.fileSystemId,
+        transitEncryption: "ENABLED",
+        authorizationConfig: {
+          accessPointId: accessPoint.accessPointId,
+          iam: "ENABLED",
+        },
+      },
+    });
+    // add the mount point to the container definition
+    sftpService.taskDefinition.defaultContainer!.addMountPoints({
+      containerPath: "/home",
+      sourceVolume: "efsVolume",
+      readOnly: false,
+    });
 
     // setup AutoScaling policy
     const sftpServiceScaling = sftpService.service.autoScaleTaskCount({
